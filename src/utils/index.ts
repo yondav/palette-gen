ca64b0da7463fc02b95d5ddb150007052d4b8b51
yondav <yoni@yondav.us>
2023-11-03 12:41:05 -0400

    chore(utils): consolidates exports of all utilities

---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export * from './types.generic';
+export * from './utils.hexToHsl';
+export * from './utils.hexToRgb';
+export * from './utils.hslToHex';
+export * from './utils.hslToRgb';
+export * from './utils.rgbToHsl';

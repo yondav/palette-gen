e64d5ed35913b6646d2e26be4f283dd9046b6341
yondav <yoni@yondav.us>
2023-11-04 10:58:04 -0400

    feat(components): adds component for displaying palette image or example vectors

---
+++ src/components/PaletteImage/index.tsx
@@ -0,0 +1,33 @@
+import { useCallback, useState } from 'react';
+
+import { ImagePalette } from '../../contexts';
+import Button from '../Button';
+import ExampleVector from '../ExampleVector';
+
+// Displays either an uploaded image or example vectors based on the state.
+export default function PaletteImage() {
+  const {
+    state: { image },
+  } = ImagePalette.use();
+
+  const [imageState, setImageState] = useState<'uploaded' | 'vectors'>('uploaded');
+
+  // Toggles between the 'uploaded' and 'vectors' states.
+  const handleImageState = useCallback(() => {
+    if (imageState === 'uploaded') setImageState('vectors');
+    if (imageState === 'vectors') setImageState('uploaded');
+  }, [imageState]);
+
+  return (
+    <div className='container'>
+      <header>
+        <Button copy={imageState} onClick={handleImageState} />
+      </header>
+      {imageState === 'uploaded' ? (
+        image.url && <img src={image.url} alt='Uploaded' className='uploaded-image' />
+      ) : (
+        <ExampleVector />
+      )}
+    </div>
+  );
+}
